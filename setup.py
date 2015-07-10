47d74c615e781faf6a705382df3bfc40571f539f
Aurelien Mazurie <ajmazurie@oenone.net>
2015-07-10 12:10:08 -0600

    [bugfix] Fixed a bug triggered when using general pathway maps (e.g., map01100) [repo] Updated setup.py to template v1

--- setup.py
+++ setup.py
@@ -1,37 +1,56 @@
 #!/usr/bin/env python
+#$changeset_latest_tag$
+#$changeset_local_revision$
 
-# 'version' follows https://www.python.org/dev/peps/pep-0440/
-
-import setuptools
-import json
 import glob
+import json
+import os
+import setuptools
 
-_SETUP = json.load(open("setup.json", "rU"))
-_VERSION = json.load(open("VERSION", "rU"))
-_REQUIREMENTS = filter(
-    lambda line: line.strip() != '',
-    open("REQUIREMENTS", "rU").readlines())
+_VERSION_SUFFIX = {
+    # development version
+    "DEVELOPMENT": "dev",
+    # pre-release versions
+    "ALPHA": "a",
+    "BETA": "b",
+    # release candidate
+    "RELEASE_CANDIDATE": "rc",
+    # final release
+    None: '.'}
+
+_CWD = os.path.abspath(os.path.dirname(__file__))
+
+def load_txt (fn):
+    if (not os.path.exists(os.path.join(_CWD, fn))):
+        return
+    for line in open(fn, "rU"):
+        line = line.strip()
+        if (line == ''):
+            continue
+        if (line[0] == '#'):
+            continue
+        yield line
+
+def load_ini (fn):
+    strip = lambda t: t.strip().strip('"').strip()
+    for line in load_txt(fn):
+        key, value = line.split('=', 1)
+        yield strip(key), strip(value)
+
+_PROJECT = dict(load_ini("PROJECT"))
+_CHANGESET = dict(load_ini("CHANGESET"))
+_REQUIREMENTS = list(load_txt("REQUIREMENTS"))
+_MANIFEST = ["PROJECT", "CHANGESET", "REQUIREMENTS"]
 
 setuptools.setup(
-    name = _SETUP["project_name"],
-    description = _SETUP.get("project_description", _SETUP["project_name"]),
-    author = _SETUP["project_author"],
-
+    name = _PROJECT["project_name"],
+    description = _PROJECT.get(
+        "project_description", _PROJECT["project_name"]),
+    author = _PROJECT["project_author"],
     version = "%s%s%s" % (
-        _VERSION["changeset_latest_tag"],
-        {
-            # development version
-            "DEVELOPMENT": "dev",
-            # pre-release versions
-            "ALPHA": "a",
-            "BETA": "b",
-            # release candidate
-            "RELEASE_CANDIDATE": "rc",
-            # final release
-            None: '.',
-        }[_SETUP.get("project_status")],
-        _VERSION["changeset_local_revision"]),
-
+        _CHANGESET["changeset_latest_tag"],
+        _VERSION_SUFFIX[_PROJECT.get("project_status")],
+        _CHANGESET["changeset_local_revision"]),
     requires = _REQUIREMENTS,
     scripts = glob.glob("bin/*"),
 )
